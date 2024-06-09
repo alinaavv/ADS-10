e785a1945e2f6598fde705a2521e5e9769a7e312
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-09 23:37:55 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -9,23 +9,16 @@ class Tree {
     char value;
     std::vector<Tree*> children;
 
-    explicit Tree(std::vector<char> elements) {
-        if (!elements.empty()) {
-            value = elements[0];
-            elements.erase(elements.begin());
-            for (char elem : elements) {
-                std::vector<char> next_elements = elements;
-                next_elements.erase(std::remove(next_elements.begin(),
-                  next_elements.end(), elem), next_elements.end());
-                children.push_back(new Tree(next_elements));
-            }
-        }
-    }
+    explicit Tree(char val) : value(val) {}
 
     ~Tree() {
         for (Tree* child : children) {
             delete child;
         }
+    }
+
+    void addChild(Tree* child) {
+        children.push_back(child);
     }
 };
 
