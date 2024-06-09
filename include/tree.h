fa9c9cc40294931a138e281be09b8fcc2e92553d
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-09 22:53:28 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,28 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+class Tree {
+public:
+    char value;
+    std::vector<Tree*> children;
+
+    Tree(std::vector<char> elements) {
+        if (!elements.empty()) {
+            value = elements[0];
+            elements.erase(elements.begin());
+            for (char elem : elements) {
+                std::vector<char> next_elements = elements;
+                next_elements.erase(std::remove(next_elements.begin(), next_elements.end(), elem), next_elements.end());
+                children.push_back(new Tree(next_elements));
+            }
+        }
+    }
+
+    ~Tree() {
+        for (Tree* child : children) {
+            delete child;
+        }
+    }
+};
+
 #endif  // INCLUDE_TREE_H_
