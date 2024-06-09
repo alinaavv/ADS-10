4cdb43e57d134ba62e64fd717eff392bbc542b35
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-09 22:57:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,27 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  int count = 0;
+    std::vector<char> permutation;
+    return getPerm(tree, n, count, permutation);
 }
+
+std::vector<char> getPerm(const Tree& tree, int n, int& count, std::vector<char>& permutation) {
+    if (tree.children.empty()) {
+        count++;
+        if (count == n) {
+            return permutation;
+        }
+    } else {
+        for (Tree* child : tree.children) {
+            permutation.push_back(child->value);
+            auto result = getPerm(*child, n, count, permutation);
+            if (!result.empty()) {
+                return result;
+            }
+            permutation.pop_back();
+        }
+    }
+  return {};
+}
+  
